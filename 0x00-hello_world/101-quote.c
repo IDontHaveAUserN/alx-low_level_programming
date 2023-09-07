d62b7814d6d18c8b3a2446ea7170f5998346e1bc
Ruth <ruthokoduwa123@gmail.com>
2023-09-07 10:42:38 -0700

    without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
 *
 *main - entry point
@@ -9,7 +8,7 @@
 */
 int main(void)
 {
-write(STDERR_FILENO, "and that piece of art is useful\
-\" - Dora Korpar, 2015-10-19\n",59);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, s, 59);
 return (1);
 }

ddd372adf6c312ad044d459927430dcbfddf7cee
Ruth <ruthokoduwa123@gmail.com>
2023-09-07 10:25:06 -0700

    without print and puts)
    )

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*main - entry point
+*
+*Return: Always 1 (success)
+*
+*/
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}

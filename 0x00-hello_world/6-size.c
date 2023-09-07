79071089377b5dd62a7fd7ccdd2ec7e6e337bedc
Ruth <ruthokoduwa123@gmail.com>
2023-09-07 10:08:46 -0700

    Size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+int a;
+char b;
+float c;
+
+printf("Size of an int: %d\n",sizeof(a));
+printf("Size of a char: %c\n",sizeof(b));
+printf("Size of a float: %f\n",sizeof(c));
+
+return (0);
+}

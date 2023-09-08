ea8893c1acda05301a1d6e25ecfc37bcff79f51f
Ruth <ruthokoduwa123@gmail.com>
2023-09-08 08:28:43 -0700

    greater and lesser than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 
 if (n > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 else if (n == 0)
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
 else

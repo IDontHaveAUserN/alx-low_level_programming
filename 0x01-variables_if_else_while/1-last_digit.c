c55ab2b418b136929482505d8142b8094345ce3c
Ruth <ruthokoduwa123@gmail.com>
2023-09-08 08:38:30 -0700

    greater and lesser than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 last_digit = n % 10;
+printf("Last digit of %d is %d and is ", n, last_digit);
 
-if (n > 5)
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
-else if (n == 0)
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
+if (last_digit > 5)
+printf("greater than 5\n");
+else if (last_digit == 0)
+printf("0\n");
 else
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("less than 6 and not 0\n");
 
 return (0);
 }
